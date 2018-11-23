82a48d5463a2d453c9211e39952420e9d734dac1
Affan <affandhia@gmail.com>
2018-11-23 09:41:54 +0700

    add updater script for updater script

--- u.sh
+++ u.sh
@@ -1,1 +1,2 @@
 curl "https://raw.githubusercontent.com/affandhia/mikrotik-auto-login/master/login.py" > l.py
+curl "https://raw.githubusercontent.com/affandhia/mikrotik-auto-login/master/u.sh" > u.sh
